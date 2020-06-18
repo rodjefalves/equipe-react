282802c9ec2d321d3f4dc4912b7f2226db8ee7c1
Jeferson <jeferson.alves@dce.ufpb.br>
2020-06-17 22:57:37 -0300

    Exemplo Finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import img1 from './img/avatar1.2.png'
+import img2 from './img/avatar1.1.png'
+import img3 from './img/avatar1.3.png'
+
+class Membro extends Component {
+  render(){
+    return(
+      <div className="col-sm">
+        <img className="imgMembro" src={this.props.img} alt="avatar do membro"/>
+        <div className="sobre">
+        <h1 className="nomeMembro">{this.props.nome}</h1>
+        <h2 className="cargoMembro">{this.props.cargo}</h2>
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jss</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerEquipe">
+      <h1 className="tituloEquipe">Nossa Equipe</h1>
+      <div className="container">
+          <div className="row">
+          <Membro nome="Maria Julia" cargo="Diretora" img={img1}/>
+          <Membro nome="Luis Henrique" cargo="Programador" img={img2}/>
+          <Membro nome="Henry Jhonson" cargo="Designer" img={img3}/>
+        </div>
+      </div>
     </div>
   );
 }
